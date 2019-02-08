6720ad07fd158aa80d83dc0c75b56923ce1e26a4
Natalie Graveson <nataliemgraveson@gmail.com>
2019-02-08 16:13:50 -0700

    working

--- game.js
+++ game.js
@@ -1,20 +1,29 @@
 let health = 100;
+let gameover = 0;
 
 
 
 function attack(damagemethod) {
   health -= (damagemethod);
-  document.getElementById('health').innerText = health
-
+  update()
 }
 function items(improvehealth) {
   health += (improvehealth);
-  document.getElementById('health').innerText = health
+  update()
 
 }
 
 function reset() {
   health = 100;
+  update()
+}
+
+function update() {
+  if (health < 0) {
+    health = 0
+  } else if (health > 100) {
+    health = 100
+  }
   document.getElementById('health').innerText = health
 }
   //draw()

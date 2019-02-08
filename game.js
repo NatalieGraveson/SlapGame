61e24f07b584fc316aa03c2a274280c7c0915630
Natalie Graveson <nataliemgraveson@gmail.com>
2019-02-08 16:03:24 -0700

    more functions

--- game.js
+++ game.js
@@ -4,10 +4,19 @@ let health = 100;
 
 function attack(damagemethod) {
   health -= (damagemethod);
-  console.log(health)
+  document.getElementById('health').innerText = health
+
+}
+function items(improvehealth) {
+  health += (improvehealth);
+  document.getElementById('health').innerText = health
 
 }
 
+function reset() {
+  health = 100;
+  document.getElementById('health').innerText = health
+}
   //draw()
 
 
